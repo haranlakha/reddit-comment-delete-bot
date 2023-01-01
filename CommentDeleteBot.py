62119b8f546cb9082ca62709ad7446b5b9850d41
haran <haran.lakha@gmail.com>
2023-01-01 19:34:14 +0000

    Intial commit

---
+++ CommentDeleteBot.py
@@ -0,0 +1,34 @@
+import praw
+from prawcore.exceptions import Forbidden
+from RedditCredentials import *
+
+USER_AGENT = "useragent goes here"
+
+USERNAME = REDDIT_USERNAME
+PASSWORD = REDDIT_PASSWORD
+CLIENT_ID = REDDIT_CLIENT_ID
+CLIENT_SECRET = REDDIT_CLIENT_SECRET
+
+SUBREDDITS = ["subreddit 1", "subreddit 2", "subreddit 3"]
+
+def remove_all_comments(reddit):
+	try:
+		count = 0
+		for comment in reddit.user.me().comments.new(limit=None):
+		    if any(subreddit.lower() == comment.subreddit.display_name.lower() for subreddit in SUBREDDITS):
+		        count += 1
+		        print("Deleting: " + comment.body)
+		        comment.delete()
+		print("Deleted " + str(count) + " comments")
+	except Exception as exception:
+		print(exception)
+
+if __name__ == "__main__":
+    reddit = praw.Reddit(
+        username=USERNAME,
+        password=PASSWORD,
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        user_agent=USER_AGENT
+    )
+    remove_all_comments(reddit)
